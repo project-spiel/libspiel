99606fc7404343b6af13a725b899ead6a48f9291
Eitan Isaacson <eitan@monotonous.org>
2023-10-07 03:11:43 +0000

    changed schema a bit

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -4,13 +4,13 @@ class SettingsTest(unittest.TestCase):
     def test_default_voice(self):
         settings = Gio.Settings.new("org.monotonous.libspiel")
 
-        self.assertEqual(settings["default-voice"], "")
-        settings["default-voice"] = "alice"
-        self.assertEqual(settings["default-voice"], "alice")
+        self.assertEqual(settings["default-voice"], None)
+        settings["default-voice"] = ("ine/hy", "org.mock2.Speech.Provider")
+        self.assertEqual(settings["default-voice"], ("ine/hy", "org.mock2.Speech.Provider"))
 
         self.assertEqual(settings["language-voice-mapping"], {})
-        settings["language-voice-mapping"] = {"en-US": "bob"}
-        self.assertEqual(settings["language-voice-mapping"], {"en-US": "bob"})
+        settings["language-voice-mapping"] = {"hy": ("ine/hyw", "org.mock2.Speech.Provider")}
+        self.assertEqual(settings["language-voice-mapping"], {"hy": ("ine/hyw", "org.mock2.Speech.Provider")})
 
 if __name__ == "__main__":
     test_main()
