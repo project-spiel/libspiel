7125ba52b9ab34fe23d7f7ac24a9f22571aa3fe3
Eitan Isaacson <eitan@monotonous.org>
2024-07-16 11:57:37 -0700

    Revert "Add warning when error is emitted."
    This reverts commit 089b58d1f4d47bef8a9928b43f93c172e2c25437.

--- libspiel/spiel-speaker.c
+++ libspiel/spiel-speaker.c
@@ -1173,7 +1173,6 @@ _advance_to_next_entry_or_finish (SpielSpeaker *self, gboolean canceled)
 
   if (entry->error)
     {
-      g_warning ("Synthesis error: %s\n", entry->error->message);
       g_signal_emit (self, speaker_signals[UTTERANCE_ERROR], 0,
                      entry->utterance, entry->error);
     }
